0e2a735d8a0f4ffb950ea885e3afbbfb29a80008
Hammy Havoc <hammy@previousmagazine.com>
2014-12-04 23:32:38 +0000

    Experimenting with WP-AppKit for Android

---
+++ config.js
@@ -0,0 +1,15 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'orion-correlation-android',
+		wp_ws_url : 'https://www.theorioncorrelation.com/app-kit-api/orion-correlation-android',
+		theme : 'nihao-android',
+		version : '1.0',
+		app_title : 'The Orion Correlation',
+		debug_mode : 'off',
+		auth_key : 'V^m7Cd1OVh8R^e1LlN3(N4Ug6tY)Crc(11C@SDYMG4UU)7Mt3JuMj9R1KNY8)TAb',
+		options : {"refresh_interval":0}	};
+
+});
